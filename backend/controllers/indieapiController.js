9f9b171a66dca3889775b37c2e9fd91c2e97acf8
Ithler The Sharp <atandagk@outlook.com>
2025-09-13 23:29:35 -0400

    Added the first model. The main model for the shows.

--- backend/controllers/indieapiController.js
+++ backend/controllers/indieapiController.js
@@ -1,41 +1,45 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
+
+const Show = require("../models/showModel");
 
 //@description: Controller for handling indie API requests.
 // @route: GET /api/indieapi
 //@access: Private
-const getData = asyncHandler(async(req, res) => {
-    res.status(200).json({ message: 'Get indie data.' });
-})
+const getData = asyncHandler(async (req, res) => {
+  const shows = await Show.find();
+
+  res.status(200).json(shows);
+});
 
 //@description: Controller for handling indie API requests.
 // @route: SET /api/indieapi
 //@access: Private
-const setData = asyncHandler(async(req, res) => {
-    if (!req.body.text) {
-        res.status(400)
-        throw new Error('Please add a text field.'); 
-    }
+const setData = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field.");
+  }
 
-    res.status(200).json({ message: 'Set indie data.' });
-})
+  res.status(200).json({ message: "Set indie data." });
+});
 
 //@description: Controller for handling indie API requests.
 // @route: PORT /api/indieapi/:id
 //@access: Private
-const updateData = asyncHandler(async(req, res) => {
-     res.status(200).json({ message: `Update indie data: ${req.params.id}.` });
-})
+const updateData = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update indie data: ${req.params.id}.` });
+});
 
 //@description: Controller for handling indie API requests.
 // @route: DELETE /api/indieapi/:id
 //@access: Private
-const deleteData = asyncHandler(async(req, res) => {
-     res.status(200).json({ message: `Delete indie data: ${req.params.id}.` });
-})
+const deleteData = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete indie data: ${req.params.id}.` });
+});
 
 module.exports = {
-    getData,
-    setData,
-    updateData,
-    deleteData
+  getData,
+  setData,
+  updateData,
+  deleteData,
 };
