a32f0a351a43ea31787eeffd883de24357a9d1f9
Ithler The Sharp <atandagk@outlook.com>
2025-09-13 23:57:53 -0400

    CRUD actions are now functional.

--- backend/controllers/indieapiController.js
+++ backend/controllers/indieapiController.js
@@ -15,26 +15,44 @@ const getData = asyncHandler(async (req, res) => {
 // @route: SET /api/indieapi
 //@access: Private
 const setData = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  if (!req.body.title) {
     res.status(400);
-    throw new Error("Please add a text field.");
+    throw new Error("Please add a title.");
   }
 
-  res.status(200).json({ message: "Set indie data." });
+  const show = await Show.create({
+    title: req.body.title,
+  });
+
+  res.status(200).json(show);
 });
 
 //@description: Controller for handling indie API requests.
 // @route: PORT /api/indieapi/:id
 //@access: Private
 const updateData = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update indie data: ${req.params.id}.` });
+  const show = await Show.findById(req.params.id);
+  if (!show) {
+    res.status(400);
+    throw new Error("Show data not found.");
+  }
+
+  const updatedShow = await Show.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedShow);
 });
 
 //@description: Controller for handling indie API requests.
 // @route: DELETE /api/indieapi/:id
 //@access: Private
 const deleteData = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete indie data: ${req.params.id}.` });
+  const show = await Show.findById(req.params.id);
+  if (!show) {
+    res.status(400);
+    throw new Error("Show data not found.");
+  }
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
